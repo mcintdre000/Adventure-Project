c227b648355cc7bb323cf3d45bf6d8a9429bd5d4
unknown <jempsey@gmail.com>
2018-07-09 19:08:50 -0700

    Added Nav Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import Adventures from "./components/Adventures/Adventures"
+import Nav from '../components/Nav/Nav';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Nav />
         <Adventures/>
       </div>
     );
