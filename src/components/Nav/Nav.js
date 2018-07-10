850ea86c6f207576e33c35ead185a437710af3f8
unknown <jempsey@gmail.com>
2018-07-09 19:11:43 -0700

    Update to Nav Component

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Nav extends Component {
+    constructor(){
+        super();
+    }
+
+    render() {
+        return (
+            <div>Nav Component</div>
+        );
+    }
+}
+
+export default Nav;
