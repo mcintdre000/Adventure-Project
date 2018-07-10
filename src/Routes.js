2eae4418abb1dbf07e882c59d3a617092bb44c8a
unknown <jempsey@gmail.com>
2018-07-10 12:13:13 -0700

    Added Routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';
+import Setting from './components/Setting/Setting';
+import Profile from './components/Profile/Profile';
+import Contact from './components/Contact/Contact';
+import Adventures from './components/Adventures/Adventures';
+import Adventure from './components/Adventure/Adventure';
 
-export default class  extends Component {
-    render() {
-        return (
-            <div>Routes</div>
-        );
-    }
-}
+export default(
+    <Switch>
+        <Route exact path='/' component={ Home } />
+        <Route exact='/login' component={ Login } />
+        <Route path='/register' component={ Register } />
+        <Route path='/profile' component={ Profile } />
+        <Route path='/contact' component={ Contact } />
+        <Route path='/adventures' component={ Adventures } />
+        <Route path='/adventure/:id' component={ Adventure } />
+    </Switch>
+)
