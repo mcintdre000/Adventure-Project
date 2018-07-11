eff708b9380446fe70276b0f2d6cdac22f714a99
unknown <jempsey@gmail.com>
2018-07-11 16:20:39 -0700

    Added comments table in init sql.

--- db/init.sql
+++ db/init.sql
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-drop table if exists users;
-create table if not exists users(
-    id serial primary key,
-    username text unique , 
-    firstName text, 
-    lastName text, 
-    email text, 
-    picture text, 
-    bio text, 
-    city text, 
-    state text, 
-    birthday text, 
-    adventures_completed text, 
-    adventure_goals text, 
-    adventures text, 
-    comments text,
-    stamp timestamp
-);
-
-Drop table if exists users_list;
-CREATE TABLE if not exists users_list(
-  id SERIAL PRIMARY KEY,
-  username VARCHAR UNIQUE,
-  password VARCHAR,
-  email VARCHAR
-);
-=======
 DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users(
     id SERIAL PRIMARY KEY,
@@ -42,9 +14,16 @@ CREATE TABLE IF NOT EXISTS users(
     adventure_goals TEXT, 
     adventures TEXT, 
     comments TEXT,
-    stamp TIMESTAMP
+    stamp TIMESTAMP,
+    comments_id int REFERENCES comments (id),
 );
 
+drop table if exists comments;
+create table if not exists comments (
+  id serial primary key,
+  content text,
+  created timestamp,
+); 
 
 --node-connect-pg-simple table.sql
 CREATE TABLE "session" (
@@ -54,4 +33,3 @@ CREATE TABLE "session" (
 )
 WITH (OIDS=FALSE);
 ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") NOT DEFERRABLE INITIALLY IMMEDIATE;
->>>>>>> feature/city-filter
