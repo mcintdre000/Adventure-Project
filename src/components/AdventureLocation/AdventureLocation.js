b8ce1b09314fa3760667b5d6c8db9d86e271db7a
unknown <jempsey@gmail.com>
2018-07-13 14:35:02 -0700

    Added API Key

--- src/components/AdventureLocation/AdventureLocation.js
+++ src/components/AdventureLocation/AdventureLocation.js
@@ -3,6 +3,8 @@ import './AdventureLocation.css';
 import AdventureMap from "./AdventureMap";
 import PropTypes from 'prop-types';
 
+const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+
 class AdventureLocation extends Component {
     render() {
         console.log('adventureLocation--', this.props)
@@ -10,7 +12,7 @@ class AdventureLocation extends Component {
             <div className="AdventureLocation">
                 <AdventureMap
                 location={this.props}
-				googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyDm9FPkFDlOwwiTmFLNR2C77-z29s75wvU&v=3.exp&libraries=geometry,drawing,places`}
+				googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3.exp&libraries=geometry,drawing,places`}
 				loadingElement={<div style={{ height: `100%` }} />}
 				containerElement={<div style={{ height: `400px`, width: `100vw` }} />}
 				mapElement={<div style={{ height: `100%` }} />}
