0d0ff81f59991e1fed7638005ccb76b2ea2df3ea
Dreier14 <37100632+Dreier14@users.noreply.github.com>
2018-07-20 17:56:22 -0700

    Friday

--- server/controllers/bcryptAuthController.js
+++ server/controllers/bcryptAuthController.js
@@ -31,7 +31,7 @@ register: (req, res) => {
             res.json(req.session.user );
           
           } else {
-            res.status(403).json({ message: 'Wrong Username or Password' });
+            res.status(403).json({ message: 'Invalid Username or Password' });
           }
         });
       } else {
