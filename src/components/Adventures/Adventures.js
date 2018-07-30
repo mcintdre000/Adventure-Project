b48b8ba8cd19e4db026e54bad95227af62f1f2b7
mcintdre000 <38018234+mcintdre000@users.noreply.github.com>
2018-07-30 13:55:21 -0700

    Photo corrections

--- src/components/Adventures/Adventures.js
+++ src/components/Adventures/Adventures.js
@@ -82,11 +82,11 @@ class Adventures extends Component {
                     <div>{ e.name } </div>
                     <div>{ e.city } </div>
                     <div>{ e.state } </div>
-                    { e.picture === ''
+                    { e.picture === null
                     ?
-                    <img src= { myPix[randomNum] } height="400px" width="400px"/>
+                    <img className='adventures-photo' src= { myPix[randomNum] } height="400px" width="400px"/>
                     :
-                    <img src= { e.picture } height="400px" width="400px"/>
+                    <img className='adventures-photo' src= { e.picture } height="400px" width="400px"/>
                     }
                     </div>
                     </Link>
@@ -100,11 +100,11 @@ class Adventures extends Component {
                     <div>{ e.name } </div>
                     <div>{ e.city } </div>
                     <div>{ e.state } </div>
-                    { e.picture === ''
+                    { e.picture === null
                     ?
-                    <img src= { myPix[randomNum] } height="400px" width="400px"/>
+                    <img className='adventures-photo' src= { myPix[randomNum] } height="400px" width="400px"/>
                     :
-                    <img src= { e.picture } height="400px" width="400px"/>
+                    <img className='adventures-photo' src= { e.picture } height="400px" width="400px"/>
                     }
                     </div>
                 </Link>
@@ -120,11 +120,11 @@ class Adventures extends Component {
                     <div>{ e.name } </div>
                     <div>{ e.city } </div>
                     <div>{ e.state } </div>
-                    { e.picture === ''
+                    { e.picture === null
                     ?
-                    <img src= { myPix[randomNum] } height="400px" width="400px"/>
+                    <img className='adventures-photo' src= { myPix[randomNum] } height="400px" width="400px"/>
                     :
-                    <img src= { e.picture } height="400px" width="400px"/>
+                    <img className='adventures-photo' src= { e.picture } height="400px" width="400px"/>
                     }
                     </div>
                 </Link>
